c3f622da7371a419d7f5b29a6d0cb7377467014f
Nopro497138 <noahjovanovic96@gmail.com>
2025-10-06 00:30:15 +0200

    Create products.js

---
+++ web/data/products.js
@@ -0,0 +1,9 @@
+// web/data/products.js
+// Edit this file to add/remove products available in the Shop.
+// Fields: id, name, description, price (points)
+
+module.exports = [
+  { id: 'ghost_hat', name: 'Ghost Hat', description: 'A spooky hat for your profile.', price: 25 },
+  { id: 'galaxy_theme', name: 'Galaxy Theme', description: 'Unlock a premium galaxy UI effect.', price: 70 },
+  { id: 'profile_badge', name: 'Profile Badge', description: 'Special badge shown on leaderboard entries.', price: 40 }
+];
